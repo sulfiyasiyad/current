3066e034e937c7b390ced5adf7c261a6142f617a
sulfiyasiyad <sulfiyacs16@gmail.com>
2024-05-30 20:28:45 +0530

    3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+ import Home from "./Home.jsx";
+import Navbar from "./Navbar.jsx";
+import Signin from "./Signin.jsx";
+import Signup from "./Signup.jsx";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+function App(){
+
+  
+  return(
+   
+    <div>
+      
+      <BrowserRouter>
+       <Navbar />
+       
+      <Routes>
+       
+        
+       
+        <Route path="" element={<Home />}></Route>
+        <Route path="/Signup" element={<Signup />}></Route>
+        <Route path="/Signin" element={<Signin />}></Route>
+     
+
+      </Routes>
+      </BrowserRouter>
+      
+      
+     
     </div>
-  );
+    
+  )
+  
+ 
+ 
+  
 }
 
 export default App;
+
+
+
---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Home(){
+    return( 
+    <div>
+        Home Page
+
+    </div>
+    );
+}
+// const Home=() =>{
+//     return <div>example</div>
+// }
+export default Home;
